3b6e1bf0819861bb1292f7a22e3edd01161bf76e
imamzafar <73028559+imamzafar@users.noreply.github.com>
2021-07-29 23:32:17 +0530

    Update urlConfig.js

--- client/src/urlConfig.js
+++ client/src/urlConfig.js
@@ -1,5 +1,7 @@
 // const baseUrl = "http://localhost:5000";
-const baseUrl =  "https://pronto-ecom.herokuapp.com";
+// const baseUrl =  "https://pronto-ecom.herokuapp.com";
+
+const baseUrl =  "https://pronto-ecomm.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
